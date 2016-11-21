b07de8514da30932b6c84198038808779abaaf29
Mark Richter <mark2685@gmail.com>
2016-11-20 21:34:35 -0800

    Updated AuthUtility class

--- src/utils/AuthUtility.js
+++ src/utils/AuthUtility.js
@@ -10,12 +10,15 @@ import { Header } from 'isomorphic-fetch'
 
 export default class AuthUtility {
   constructor(headers = {}) {
+    this.cache = []
+
     this.headers = headers
   }
-  static setBearerToken(headers, token) {
-    return Object.assign({}, headers, {
-      Authorization: `Bearer ${token}`
-    })
+  set cache(cache = []) {
+    this._cache = cache
+  }
+  get cache() {
+    return this._cache
   }
   set isRefreshingTokens(bool) {
     this._isRefreshingTokens = bool
@@ -25,14 +28,25 @@ export default class AuthUtility {
   }
   set accessToken(token) {
     this._accessToken = token
+
+    this.headers = Object.assign({}, this.headers, {
+      Authorization: `Bearer ${token}`
+    })
   }
   get accessToken() {
     return this._accessToken
   }
-  set headers(headers) {
-    this._headers = this.setBearerToken(headers, this.accessToken)
+  set headers(headers = { 'Content-Type': 'application/json' }) {
+    this._headers = headers
   }
   get headers() {
     return this._headers
+  }
+  cacheAction(request) {
+    if (this.cache[request.url]) {
+      this.cache[request.url] = request.url
+      this.cache[request.url].push()
+    }
+    this.cache
   }
 }
